360a61e2ace0de62eb4c54ad3fc282b9d42afedf
Stas Oskol <stas-stas.stanislav@mail.ru>
2025-05-15 23:07:28 +0300

    добавление приложения frontend

---
+++ fullstack-frontend/src/router/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import MainPage from '../pages/MainPage';
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path="/" index element={<MainPage />} />
+    </Routes>
+  );
+};
+
+export default Router;
