17462334b90afaeca13a7ae0e273c011ba3dac15
Stas Osk <stas-stas.stanislav@mail.ru>
2025-05-18 17:31:52 +0300

    добавил компонент для работой put path delete

---
+++ fullstack-frontend/src/pages/ChangeDataBackend.tsx
@@ -0,0 +1,12 @@
+import {
+    Box
+} from "@mui/material";
+
+
+const ChangeDataBackend = () => {
+    return <Box>
+        
+    </Box>
+}
+
+export default ChangeDataBackend;
--- fullstack-frontend/src/router/Router.tsx
+++ fullstack-frontend/src/router/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import MainPage from '../pages/MainPage';
+import ChangeDataBackend from '../pages/ChangeDataBackend';
 
 const Router = () => {
   return (
     <Routes>
-        <Route path="/" index element={<MainPage />} />
+      <Route path="/" index element={<MainPage />} />
+      <Route path="/change-backend" index element={<ChangeDataBackend />} />
     </Routes>
   );
 };
