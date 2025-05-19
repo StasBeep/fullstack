4b31e3a246131ae920a747c5b07f87a3aa4be920
Stas Oskol <stas-stas.stanislav@mail.ru>
2025-05-19 22:50:19 +0300

    Изменение проектирования api по модулям 1. Добавление строчки в package.json: "type": "module" 2. При import обязательно нужно использовать расширение (.js или другие) -  иначе ошибка 3. require перестаёт работать, но можно подключить через createRequire в библиотеке module 4. Можно теперь раскидывать логику api в разные разделы и работать с разными .json

---
+++ fullstack-backend/_dataTest.js
@@ -0,0 +1,39 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const data = require('./data.json');
+
+export const dataTest = (app) => {
+    app.get('/api/data/:id', (req, res) => {
+        const idDataReq = req.params.id;
+        console.log('start request id: ' + idDataReq);
+
+        const dataResponse = data.dataTest.find((item) => item.id === parseInt(idDataReq));
+
+        if (!dataResponse) {
+            console.log('No id: ' + idDataReq);
+            return res.status(404).send("Data not found");
+        } else {
+            res.json(dataResponse);
+        }
+    });
+
+    app.put('/api/data/:id', (req, res) => {
+        console.log('change data for id: ' + req.params.id);
+        const idDataReq = req.params.id;
+        const updatedData = req.body; //! Не сработает без - app.use(express.json());
+
+        let indexDataTest = data.dataTest.findIndex((item) => item.id === parseInt(idDataReq));
+
+        console.log(indexDataTest);
+
+        if (indexDataTest === -1) {
+            console.log('No id: ' + idDataReq);
+            return res.status(404).send("Data not found");
+        } else {
+            data.dataTest[indexDataTest] = updatedData;
+            console.log("completed change data");
+            res.json(data.dataTest[indexDataTest]);
+        }
+    });
+}
--- fullstack-backend/index.js
+++ fullstack-backend/index.js
@@ -1,10 +1,16 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const express = require("express");
-const data = require('./data.json');
+
+import { dataTest } from "./_dataTest.js";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+
+dataTest(app);
 
 app.listen(PORT, () => {
 	console.log(`Server starting on port ${PORT}`);
@@ -14,37 +20,4 @@ app.get('/api', (req, res) => {
 	res.json({
 		message: 'Hello from backend server'
 	})
-});
-
-app.get('/api/data/:id', (req, res) => {
-	const idDataReq = req.params.id;
-	console.log('start request id: ' + idDataReq);
-
-	const dataResponse = data.dataTest.find((item) => item.id === parseInt(idDataReq));
-
-	if (!dataResponse) {
-		console.log('No id: ' + idDataReq);
-		return res.status(404).send("Data not found");
-	} else {
-		res.json(dataResponse);
-	}
-});
-
-app.put('/api/data/:id', (req, res) => {
-	console.log('change data for id: ' + req.params.id);
-	const idDataReq = req.params.id;
-	const updatedData = req.body; //! Не сработает без - app.use(express.json());
-
-	let indexDataTest = data.dataTest.findIndex((item) => item.id === parseInt(idDataReq));
-
-	console.log(indexDataTest);
-
-	if (indexDataTest === -1) {
-		console.log('No id: ' + idDataReq);
-		return res.status(404).send("Data not found");
-	} else {
-		data.dataTest[indexDataTest] = updatedData;
-		console.log("completed change data");
-		res.json(data.dataTest[indexDataTest]);
-	}
 });
