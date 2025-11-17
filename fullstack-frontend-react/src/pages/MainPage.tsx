ce654fed3ef48bee66b69e0dc47ac77c35aa0fc5
Stas Osk <stas-stas.stanislav@mail.ru>
2025-11-17 12:35:32 +0300

    напоминание

--- fullstack-frontend-react/src/pages/MainPage.tsx
+++ fullstack-frontend-react/src/pages/MainPage.tsx
@@ -4,23 +4,23 @@ import { useNavigate } from "react-router-dom";
 import { getCommon } from "../api/controllers/common-contreller";
 
 import {
-  Box,
-  Button,
-  Typography
+  Box
 } from "@mui/material";
 
 const MainPage = () => {
   const navigate = useNavigate();
 
-  const [data, setData] = React.useState();
+  const [data, setData] = React.useState<string>('Нет связи с сервером');
 
   useEffect(() => {
     getCommon()
       .then((response) => {
         console.log(response);
         setData(response.data.message);
+        navigate(-1);
       })
       .catch((e) => console.log(e));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Box
@@ -29,7 +29,7 @@ const MainPage = () => {
       m: '0 auto'
     }}
   >
-    
+    {data}
   </Box>
 };
 
